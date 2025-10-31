3324427c3cc5c9ca11cb23c3309d1e016a4fc1ec
Ishara Palangasinghe <145181123+ishara425@users.noreply.github.com>
2025-10-31 22:10:35 +0530

    Remove backend management module and all related files for the Medicine Stock Management System. This includes the pom.xml, application code, models, repositories, services, controllers, security configurations, and SQL data. The entire structure has been deleted to facilitate a complete overhaul or migration of the project.

--- backend/management/src/main/java/com/medicinestock/management/repository/DistributionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.medicinestock.management.repository;
-
-import com.medicinestock.management.model.Distribution;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DistributionRepository extends JpaRepository<Distribution, Long> {
-}
